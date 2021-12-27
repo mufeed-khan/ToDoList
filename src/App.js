697ad882dadac55e69047739d48d47f1742ff48f
mufeed khan <mufeedkhan192@gmail.com>
2021-12-27 22:15:56 +0530

    initail commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  const [inputData, setInputData] = useState("");
+  const [item, setItem] = useState([]);
+
+  const handleChange = (e) => {
+    setInputData(e.target.value);
+  };
+
+
+  //add item
+  const additem = () => {
+    if (!inputData) {
+    } else {
+      setItem([...item, inputData]);
+      setInputData("");
+    }
+  };
+
+  //delete item
+  const deleteItem = (id) => {
+  
+    const updateitems = item.filter((elem, ind) => {
+      return ind !== id;
+    });
+    setItem(updateitems);
+  };
+
+  //clear all
+  const removeAll=()=>{
+    setItem([])
+  }
+
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setInputData("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <form onSubmit={handleSubmit}>
+          <div>
+            <div>
+              <h3>What is the plane today ?</h3>
+              <input
+                type="text"
+                placeholder="Add Items..."
+                value={inputData}
+                name="text"
+                onChange={handleChange}
+              />
+              <button onClick={additem}>Add</button>
+              <br></br>
+
+              <div>
+                {item.map((elem, ind) => {
+                  return (
+                    <div key={ind}>
+                      {elem}
+                      <br />
+
+                      <button onClick={() => deleteItem(ind)}>Delete</button>
+                    </div>
+                  );
+                })}
+              </div>
+
+              <br />
+              <br />
+              <br />
+
+
+              <button onClick={removeAll}>Clear All</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
+
